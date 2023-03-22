6b4d166ced501290bf2153fce58a7914dbcf21ee
Abhijeet Singh <43774040+geekyAbhijeetSr@users.noreply.github.com>
2023-03-22 16:01:41 +0530

    .

--- src/config/mailTransporter.js
+++ src/config/mailTransporter.js
@@ -1,12 +1,12 @@
 const nodemailer = require('nodemailer')
 
 let mailTransporter = nodemailer.createTransport({
-	host: 'smtp.gmail.com',
+	host: process.env.NODE_MAILER_HOST,
 	port: 465,
 	secure: true,
 	auth: {
-		user: 'nor666108@gmail.com',
-		pass: 'quqryqqxklxcppdy',
+		user: process.env.NODE_MAILER_USER,
+		pass: process.env.NODE_MAILER_PASS,
 	},
 })
 
